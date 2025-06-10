4a73df605f1641217a9e5aa46229987a346a636a
Mariofte <150403921+Mariofte@users.noreply.github.com>
2025-06-09 22:03:38 -0600

    JSJSJSJS

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import *
+from flask import Flask, render_template, redirect, url_for, request
 from service import spread_sheet
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
-api = spread_sheet(id='1-w-sizFV2i0BTeEd-PzQ9oQojd3A2Y_pvm2XJ4e35z8')
+api = spread_sheet(id='')
 
-"""
-    El archivo index.html y match.html no existen ahorita
-"""
+def page_error(error):
+    return render_template('404.html'), 404
 
 @app.route('/')
 def index():
@@ -15,8 +14,21 @@ def index():
 @app.route('/App/match', methods=['GET','POST'])
 def match():
     if request.form == 'POST':
-        DATA = request.form.getlist('data[]')
-        api.post([[DATA]], 2, 0)
+        data = request.form.getlist('data[]')
+        api.post([[data]], 2, 0)
         return redirect(url_for('match'))
     
-    return render_template('match.html')
+    return render_template('match.html', data=api.get(2))
+
+@app.route('/App/pits', methods=['GET','POST'])
+def pits():
+    if request.form == 'POST':
+        data = request.form.getlist('data[]')
+        api.post([[data]], 2, 1)
+        return redirect(url_for('pits.html'))
+    
+    return render_template('pits.html', data=api.get(2))
+
+if __name__ == '__main__':
+    app.register_error_handler(404, page_error)
+    app.run(host='localhost', debug=True, port=5000)
